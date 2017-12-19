fcb354386eb0a7c73faac193b2b611c54fcc4b7f
Toni Stachewicz <toni.stachewicz@student.hpi.de>
2017-12-19 01:18:31 +0100

    introduced logging

--- blockchain/block.py
+++ blockchain/block.py
@@ -1,11 +1,19 @@
+import logging
 from hashlib import sha256
 from time import time
 
 from .config import CONFIG
 
+# Needs to be moved later
+logging.basicConfig(level=logging.DEBUG,
+                    format='[ %(asctime)s ] %(levelname)-7s %(name)-s: %(message)s',
+                    datefmt="%Y-%m-%d %H:%M:%S")
+logger = logging.getLogger('blockchain')
+
 
 class Block(object):
     def __init__(self, dictionary):
+        logger.debug('Creating new block')
         self.version = CONFIG['version']
         self.previous_block = dictionary['hash']
         self.merkle = dictionary['merkle']
@@ -16,12 +24,14 @@ class Block(object):
 
     def __str__(self):
         return ('=======================\n'
-                '  Block {},\n'
-                '  Previous block: {},\n'
+                '  Block {}\n'
+                '  Previous block: {}\n'
+                '  Merkle root: {}\n'
                 '  Number of transactions: {}\n'
                 '  hash: {}\n'
                 '=======================').format(self.index,
                                                   self.previous_block,
+                                                  self.merkle,
                                                   len(self.transactions),
                                                   self.hash)
 
@@ -49,13 +59,17 @@ class Block(object):
             'hash': self.hash
         }
 
+    # Add hash to the final state of the block
     def update_hash(self):
         sha = sha256()
         sha.update(str(self._get_relevant_data_for_hashing()).encode('utf-8'))
         self.hash = sha.hexdigest()
+        logger.debug('Finished creation of block:\n{}'.format(str(self)))
 
 
 def create_initial_block():
+    '''Creates the genesis block.'''
+    logger.info('Creating new genesis block')
     return Block({
         'previous_block': 0,
         'merkle': 0,
