70e96ea0b48816b81e9c2861285e760c8096db47
alpreu <alex@Alex-MacBook.local>
2018-01-09 11:40:17 +0100

    Refactoring crypto helper to match meeting requirements

--- blockchain/helper/cryptography.py
+++ blockchain/helper/cryptography.py
@@ -7,18 +7,16 @@ def get_bytes(message):
     return bytes(message, encoding="utf8")
 
 def generate_keypair(keysize=2048):
-    """
-    generates a new keypair using a random seed and the RSA algorith
-    """
+    """Generate a new keypair using a random seed and the RSA algorithm."""
     random = Random.new().read
     key = RSA.generate(keysize, random)
     public, private  = key.publickey(), key
     return public, private
 
 def sign(message, private_key):
-    """
-    creates a signature for the message with the given private key,
-    expects message as a byte string
+    """Create a cryptographic signature for the given message and private key.
+
+    Expects message as a byte string, use get_bytes() to convert.
     """
     signer = PKCS1_v1_5.new(private_key)
     digest = SHA256.new()
@@ -26,15 +24,15 @@ def sign(message, private_key):
     return signer.sign(digest)
 
 def verify(message, signature, public_key):
+    """Verifies the message with the given signature and public key.
+
+    Expects message as a byte string, use get_bytes() to convert.
+    """
     signer = PKCS1_v1_5.new(public_key)
-    """
-    verifies the message with the given signature and public key,
-    expects message as a byte string
-    """
     digest = SHA256.new()
     digest.update(message)
     return signer.verify(digest, signature)
 
 
 def hexify(s):
-    return s.hex() if s else ''
+    return s.hex() if s else ""
