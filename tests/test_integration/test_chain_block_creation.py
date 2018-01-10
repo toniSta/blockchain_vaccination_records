af7e42a31a6af73414519c3ece4befa8b0ac997c
Toni Stachewicz <toni.stachewicz@student.hpi.de>
2018-01-10 23:45:47 +0100

    added integration test for chain and block creation

---
+++ tests/test_integration/test_chain_block_creation.py
@@ -0,0 +1,33 @@
+from blockchain.block import *
+from blockchain.chain import Chain
+from blockchain.transaction import *
+from Crypto.PublicKey import RSA
+
+
+def test_chain_is_singleton():
+    with open("tests" + os.sep + "testkey_pub.bin", "rb") as public_key, open("tests" + os.sep + "testkey_priv.bin", "rb") as private_key:
+        PUBLIC_KEY = RSA.import_key(public_key.read())
+        PRIVATE_KEY = RSA.import_key(private_key.read())
+
+    # Create chain, already contains empty genesis
+    chain = Chain()
+
+    # new Block with transactions
+    new_block = Block(chain.last_block().get_block_information())
+    new_transaction = VaccineTransaction("a vaccine", PUBLIC_KEY).sign(PRIVATE_KEY)
+    new_block.add_transaction(new_transaction)
+    new_transaction = PermissionTransaction(Permission.doctor, PUBLIC_KEY).sign(PRIVATE_KEY)
+    new_block.add_transaction(new_transaction)
+
+    new_block.update_hash()
+    new_block.persist()
+
+    # read file from disk
+    blockchain_folder = "blockchain"
+    persistence_folder = os.path.join(blockchain_folder,
+                                      CONFIG["persistance_folder"])
+    with open(os.path.join(persistence_folder, "1"), "r") as file:
+        recreated_block = Block(file.read())
+
+    # can build new block based on recreated block
+    Block(recreated_block.get_block_information())
