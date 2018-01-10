ba61438422f3c004794c35e166df6bb94f810191
Benedikt Bock <mail@benedikt1992.de>
2018-01-10 21:33:33 +0100

    bugfix

--- blockchain/transaction/__init__.py
+++ blockchain/transaction/__init__.py
@@ -1,1 +1,5 @@
+from .permission_transaction import PermissionTransaction
+from .vaccination_transaction import VaccinationTransaction
+from.vaccine_transaction import VaccineTransaction
+
 __all__ = ["PermissionTransaction", "VaccinationTransaction", "VaccineTransaction"]
