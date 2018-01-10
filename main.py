501ee5f314e640ccc3a4af13a9bd6098bcd30c1a
Toni Stachewicz <toni.stachewicz@student.hpi.de>
2018-01-10 23:45:15 +0100

    extended demo script by using chain class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """This file is for playing around. Feel free to alter it."""
 
 from blockchain.block import *
+from blockchain.chain import Chain
 from blockchain.transaction import *
 from Crypto.PublicKey import RSA
 
@@ -9,19 +10,30 @@ if __name__ == "__main__":
     with open("tests" + os.sep + "testkey_pub.bin", "rb") as public_key, open("tests" + os.sep + "testkey_priv.bin", "rb") as private_key:
         PUBLIC_KEY = RSA.import_key(public_key.read())
         PRIVATE_KEY = RSA.import_key(private_key.read())
-    genesis = create_initial_block()
-    new_transaction = VaccineTransaction('a vaccine', PUBLIC_KEY).sign(PRIVATE_KEY)
-    genesis.add_transaction(new_transaction)
-    new_transaction = PermissionTransaction(Permission.doctor,PUBLIC_KEY).sign(PRIVATE_KEY)
-    genesis.add_transaction(new_transaction)
-    genesis.update_hash()
-    # print(repr(asd))
-    genesis.persist()
+
+    # Create chain, already contains empty genesis
+    chain = Chain()
+
+    # new Block with transactions
+    new_block = Block(chain.last_block().get_block_information())
+    new_transaction = VaccineTransaction("a vaccine", PUBLIC_KEY).sign(PRIVATE_KEY)
+    new_block.add_transaction(new_transaction)
+    new_transaction = PermissionTransaction(Permission.doctor, PUBLIC_KEY).sign(PRIVATE_KEY)
+    new_block.add_transaction(new_transaction)
+
+    new_block.update_hash()
+    new_block.persist()
+
+    # read file from disk
     blockchain_folder = "blockchain"
     persistence_folder = os.path.join(blockchain_folder,
                                       CONFIG["persistance_folder"])
-    with open(os.path.join(persistence_folder, '0'), 'r') as file:
+    with open(os.path.join(persistence_folder, "1"), "r") as file:
         recreated_block = Block(file.read())
 
-    print(genesis)
+    print(chain.find_block_by_index(0))
+    print(new_block)
     print(recreated_block)
+
+    # can build new block based on recreated block
+    Block(recreated_block.get_block_information())
