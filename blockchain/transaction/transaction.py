930e3d6a2b5cd82484e2aadeb1412f1b79a06dbf
alpreu <alex@Alex-MacBook.fritz.box>
2018-01-11 18:09:32 +0100

    changing quote style

--- blockchain/transaction/transaction.py
+++ blockchain/transaction/transaction.py
@@ -6,8 +6,8 @@ from time import time
 class TransactionBase(metaclass=ABCMeta):
 
     def __init__(self, *args, **kwargs):
-        self.version = kwargs.get('version') or CONFIG['version']
-        self.timestamp = kwargs.get('timestamp') or int(time())
+        self.version = kwargs.get("version") or CONFIG["version"]
+        self.timestamp = kwargs.get("timestamp") or int(time())
 
     @abstractmethod
     def validate(self):
@@ -27,10 +27,10 @@ class TransactionBase(metaclass=ABCMeta):
         """
         instance_member_list = []
         for item in vars(self).items():
-            if type(item[1]).__name__ == 'bytes':
+            if type(item[1]).__name__ == "bytes":
                 instance_member_list.append((item[0].title(), hexify(item[1])))
                 continue
-            if type(item[1]).__name__ == 'list':
+            if type(item[1]).__name__ == "list":
                 hex_list = [hexify(e) for (e, _) in item[1] if type(e) == "bytes"]
                 instance_member_list.append((item[0].title(), hex_list))
                 continue
@@ -38,11 +38,11 @@ class TransactionBase(metaclass=ABCMeta):
             instance_member_list.append((item[0].title(), item[1]))
         instance_member_list.sort(key=lambda tup: tup[0])
 
-        string = '-----------------------\n'
-        string = string + '  Transaction: {}\n'.format(type(self).__name__)
+        string = "-----------------------\n"
+        string = string + "  Transaction: {}\n".format(type(self).__name__)
         for tuple in instance_member_list:
-            string = string + '  {}: {}\n'.format(*tuple)
-        string = string + '-----------------------'
+            string = string + "  {}: {}\n".format(*tuple)
+        string = string + "-----------------------"
         return string
 
 
@@ -50,14 +50,14 @@ class TransactionBase(metaclass=ABCMeta):
         """
         This method returns a string representation of the object such that eval() can recreate the object.
         The Class attributes will be ordered
-        e.g. Class(attribute1='String', attribute2=3)
+        e.g. Class(attribute1="String", attribute2=3)
         """
         instance_member_list =[]
         for item in vars(self).items():
             instance_member_list.append(item)
         instance_member_list.sort(key=lambda tup: tup[0])
 
-        return '{!s}({!s})'.format(
+        return "{!s}({!s})".format(
             type(self).__name__,
-            ', '.join(['{!s}={!r}'.format(*item) for item in instance_member_list])
+            ", ".join(["{!s}={!r}".format(*item) for item in instance_member_list])
         )
