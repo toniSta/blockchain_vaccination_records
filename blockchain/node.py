43895ffb33132a09ac04762a5e489ebbbac91488
alpreu <alex@Alex-MacBook.fritz.box>
2018-01-14 16:11:43 +0100

    first prototype of tx creation dialogue

--- blockchain/node.py
+++ blockchain/node.py
@@ -2,7 +2,7 @@ import logging
 
 import random
 import requests
-from orderedset import OrderedSet
+from transaction_set import TransactionSet
 from .block import Block
 from .chain import Chain
 from .config import CONFIG
@@ -15,21 +15,10 @@ class Node(object):
         # Mock nodes by hard coding
         self.nodes = ["http://127.0.0.1:9000"]
         self.chain = Chain()
-        # Transaction set needs to be implemented, right out it is just a set
-        self.transaction_set = OrderedSet()
+        self.transaction_set = TransactionSet()
         self.invalid_transactions = set()
 
         self.recover_after_shutdown()
-
-    def handle_new_transaction(self, transaction):
-        transaction_object = eval(transaction)
-        # We only want to broadcast a tx if it already is in the queue or in the chain
-        try:
-            self.transaction_set.index(transaction_object)
-        except ValueError:
-            self.transaction_set.add(transaction_object)
-            # TODO: check if it is in the chain already
-            self._broadcast_new_transaction(transaction)
 
     def synchronize_blockchain(self):
         random_node = random.choice(self.nodes)
@@ -78,11 +67,6 @@ class Node(object):
             route = node + "/new_block"
             requests.post(route, data=repr(block))
 
-    def _broadcast_new_transaction(self, transaction):
-        for node in self.nodes:
-            route = node + "/new_transaction"
-            requests.post(route, data=repr(transaction))
-
     def _get_status_from_different_node(self, node):
         random_node = random.choice(self.nodes)
         route = random_node + "/latest_block"
@@ -94,3 +78,70 @@ class Node(object):
         #   1. read in files from disk -> maybe in __init__ of chain
         #   2. sync with other node(s)
         pass
+
+    def handle_new_transaction(self, transaction):
+        transaction_object = eval(transaction)
+        if self.transaction_set.contains(transaction_object):
+            return  # Transaction was already received
+        else:
+            # TODO: check if it is in the chain already
+            self.transaction_set.add(transaction_object)
+            self._broadcast_new_transaction(transaction)
+
+    def _broadcast_new_transaction(self, transaction):
+        for node in self.nodes:
+            route = node + "/new_transaction"
+            requests.post(route, data=repr(transaction))
+
+    def create_transaction(self):
+        transaction_type = input("What kind of transaction should be created? (Vaccination/Vaccine/Permission)").lower()
+        if transaction_type == "vaccination":
+            vaccine = input("Which vaccine was given?").lower()
+            doctor_pubkey = input("Enter doctors public key")
+            patient_pubkey = input("Enter patients public key")
+            transaction = VaccinationTransaction(doctor_pubkey, patient_pubkey, vaccine)
+            print(transaction)
+            sign_now = input("Sign transaction now? (Y/N)").lower()
+            if sign_now == "y":
+                doctor_privkey = input("Enter doctors private key")
+                patient_privkey = input("Enter patients private key")
+                transaction.sign(doctor_privkey, patient_privkey)
+                print(transaction)
+                return transaction
+            elif sign_now == "n":
+                return transaction
+            else:
+                print("Invalid option {}, aborting.".format(sign_now))
+        elif transaction_type == "vaccine":
+            vaccine = input("Which vaccine should be registered?").lower()
+            admission_pubkey = input("Enter admissions public key")
+            transaction = VaccineTransaction(vaccine, admission_pubkey)
+            print(transaction)
+            sign_now = input("Sign transaction now? (Y/N)").lower()
+            if sign_now == "y":
+                admission_privkey = input("Enter admission private key")
+                transaction.sign(admission_privkey)
+                print(transaction)
+                return transaction
+            elif sign_now == "n":
+                return transaction
+            else:
+                print("Invalid option {}, aborting.".format(sign_now))
+        elif transaction_type == "permission":
+            permission_name = input("Which permission should be granted? (Patient/Doctor/Admission)").lower()
+            permission = Permission[permission_name]
+            sender_pubkey = input("Enter sender public key")
+            transaction = PermissionTransaction(permission, sender_pubkey)
+            print(transaction)
+            if sign_now == "y":
+                sender_privkey = input("Enter sender private key")
+                transaction.sign(sender_privkey)
+                print(transaction)
+                return transaction
+            elif sign_now == "n":
+                return transaction
+            else:
+                print("Invalid option {}, aborting.".format(sign_now))
+        else:
+            print("Invalid option {}, aborting.".format(transaction_type))
+
