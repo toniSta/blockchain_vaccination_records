fffaaf7f25f8d54fe9fce7f08e748814f3a2401f
Toni Stachewicz <toni.stachewicz@student.hpi.de>
2018-01-22 02:17:56 +0100

    made network methods static

--- blockchain/network.py
+++ blockchain/network.py
@@ -13,19 +13,22 @@ from .config import CONFIG
 class Network(ABCMeta):
     """Facade for network operations."""
 
-    def send_block(self, node, block_data):
+    @staticmethod
+    def send_block(node, block_data):
         """Send a block to the specified node."""
         # TODO: this doesnt work, if we send it to the same node
         return
         route = node + CONFIG["ROUTES"]["new_block"]
         requests.post(route, data=block_data, timeout=5)
 
-    def request_latest_block(self, node):
+    @staticmethod
+    def request_latest_block(node):
         """Ask another node for current chain status."""
         route = node + CONFIG["ROUTES"]["latest_block"]
         return requests.get(route)
 
-    def broadcast_new_transaction(self, node, transaction):
+    @staticmethod
+    def broadcast_new_transaction(node, transaction):
         """Broadcast a transaction to neighbours."""
         route = node + CONFIG["ROUTES"]["new_transaction"]
         requests.post(route, data=transaction)
