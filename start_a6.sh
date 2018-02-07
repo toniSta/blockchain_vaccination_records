bdc06742dce66c8d29e64d5e61caea21c10b01f8
Benedikt Bock <mail@benedikt1992.de>
2018-02-07 20:23:12 +0100

    add node a6

---
+++ start_a6.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+dir_name=${PWD##*/}
+dir_name_stripped=${dir_name//_}
+docker run --name a6 -it -p 9000 --network ${dir_name_stripped}_default -e "REGISTER_AS_ADMISSION=1"  -e "CONFIRM_BLOCKSENDING=1" -e "NEIGHBORS_HOST_PORT=a2:9000,genesis_admission:9000" full_client_image
