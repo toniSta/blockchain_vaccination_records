033e417420a60b3d08316fdb355136a2228acd45
Benedikt Bock <mail@benedikt1992.de>
2018-02-16 13:56:50 +0100

    connect node d3 with every admission node

--- start_d3.sh
+++ start_d3.sh
@@ -3,4 +3,4 @@ dir_name=${PWD##*/}
 dir_name_stripped=${dir_name//_}
 mytitle="d3"
 echo -e '\033]2;'$mytitle'\007'
-docker run --name d3 -it -p 9000 --network ${dir_name_stripped}_default -e "START_CLI=1" -e "NEIGHBORS_HOST_PORT=d1:9000,a2:9000,d2:9000" full_client_image
+docker run --name d3 -it -p 9000 --network ${dir_name_stripped}_default -e "START_CLI=1" -e "NEIGHBORS_HOST_PORT=d1:9000,a2:9000,d2:9000,genesis_admission:9000,a1:9000,a3:9000" full_client_image
