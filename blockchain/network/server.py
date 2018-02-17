8d119ac93b447f93660e2259cb94eaef137c4d6d
Benedikt Bock <mail@benedikt1992.de>
2018-02-17 17:51:43 +0100

    rearange server

--- blockchain/network/server.py
+++ blockchain/network/server.py
@@ -5,12 +5,12 @@ Needs to be replaced, when a P2P is estabished.
 """
 import threading
 from time import sleep
-
 from flask import Flask, request
 import os
 from threading import Thread
-from ..config import CONFIG
 import logging
+
+from ..config import CONFIG
 from blockchain.helper.logger import setup_basic_logger_config
 
 setup_basic_logger_config()
@@ -19,32 +19,17 @@ logger = logging.getLogger('server')
 app = Flask(__name__)
 
 
-def handle_received_block(block):
-    """Handle new block in extra thread for early return."""
-    full_client.received_new_block(block)
-
-
-def handle_received_transaction(transaction):
-    """Handle new transaction in extra thread for early return."""
-    full_client.handle_incoming_transaction(transaction)
-
-
-def handle_received_judgement(judgement):
-    """Handle new judgement in extra thread for early return."""
-    full_client.handle_received_judgement(judgement)
-
-
-def handle_sync_request(data):
-    sender_host = data[0]
-    block = data[1]
-    full_client.handle_sync_request(sender_host, block)
-
-
 @app.route(CONFIG["ROUTES"]["new_block"], methods=["POST"])
 def _new_block():
     block = request.data.decode("utf-8")
     Thread(target=handle_received_block, args=(block,), daemon=True, name="handle_received_block_thread").start()
     return "success"
+
+
+def handle_received_block(block):
+    """Handle new block in extra thread for early return."""
+    full_client.received_new_block(block)
+
 
 @app.route(CONFIG["ROUTES"]["block_by_hash"], methods=["GET"])
 def _send_block_by_hash(hash):
@@ -58,6 +43,11 @@ def _new_transaction():
     return "success"
 
 
+def handle_received_transaction(transaction):
+    """Handle new transaction in extra thread for early return."""
+    full_client.handle_incoming_transaction(transaction)
+
+
 @app.route(CONFIG["ROUTES"]["new_judgement"], methods=["POST"])
 def _new_judgement():
     new_judgement = request.data
@@ -65,11 +55,22 @@ def _new_judgement():
     return "success"
 
 
+def handle_received_judgement(judgement):
+    """Handle new judgement in extra thread for early return."""
+    full_client.handle_received_judgement(judgement)
+
+
 @app.route(CONFIG["ROUTES"]["sync_request"], methods=["POST"])
 def _sync_request():
     data = eval(request.data.decode("utf-8")) # Don't do this in real life!
     Thread(target=handle_sync_request, args=(data,), daemon=True, name="handle_sync_request_thread").start()
     return "success"
+
+
+def handle_sync_request(data):
+    sender_host = data[0]
+    block = data[1]
+    full_client.handle_sync_request(sender_host, block)
 
 
 def start_server(client):
