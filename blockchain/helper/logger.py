f2f56f64cb54311446498ff7f6b648d4b04fa785
Benedikt Bock <mail@benedikt1992.de>
2018-02-17 17:42:45 +0100

    specify logger format

--- blockchain/helper/logger.py
+++ blockchain/helper/logger.py
@@ -4,8 +4,9 @@ from blockchain.config import CONFIG
 
 
 def setup_basic_logger_config():
-    logging.basicConfig(level=CONFIG["loglevel"])
-
+    logging.basicConfig(level=CONFIG["loglevel"],
+                        format="[ %(asctime)s ] %(levelname)-7s %(name)-s: %(message)s",
+                        datefmt="%Y-%m-%d %H:%M:%S")
 
 def write_logs_to_file():
     logger = logging.getLogger("LogSetup")
@@ -13,4 +14,8 @@ def write_logs_to_file():
     logger.info("Set log output to file {}".format(file_path))
     os.makedirs(os.path.dirname(file_path))
     [logging.root.removeHandler(handler) for handler in logging.root.handlers[:]]
-    logging.basicConfig(level=CONFIG["loglevel"], filename=file_path, filemode='a+')
+    logging.basicConfig(level=CONFIG["loglevel"],
+                        format="[ %(asctime)s ] %(levelname)-7s %(name)-s: %(message)s",
+                        datefmt="%Y-%m-%d %H:%M:%S",
+                        filename=file_path,
+                        filemode='a+')
