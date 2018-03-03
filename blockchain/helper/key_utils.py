2d7738664d06305ae4518cf3eaf006e4f92b710a
alpreu <alex@Alex-MacBook.fritz.box>
2018-03-03 17:15:22 +0100

    initial prototype

---
+++ blockchain/helper/key_utils.py
@@ -0,0 +1,21 @@
+"""This module provides helper functions for handling private/public keys.
+"""
+
+
+def bytestring_to_rsa(key):
+    pass
+
+def bytestring_to_hex(key):
+    pass
+
+def hex_to_bytestring(key):
+    pass
+
+def hex_to_rsa(key):
+    pass
+
+def rsa_to_bytestring(key):
+    pass
+
+def rsa_to_hex(key):
+    pass
